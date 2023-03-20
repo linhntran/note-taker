16101f483250d6585571773fb2042c4297bfb7dc
Linh Tran <linhtran@Linhs-MacBook-Air.local>
2023-03-19 18:43:03 -0700

    updated notes and added Procfile for heroku

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { createNewNote, deleteNote } = require('../../lib/notes');
-let { notesArray } = require('../../db/notes');
+const { createNewNote, deleteNote } = require('./lib/notes');
+let { notesArray } = require('./db/notes');
 
 router.get('/notes', (req, res) => {
   let results = notesArray;
