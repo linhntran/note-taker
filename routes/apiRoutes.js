8861e4ed5c21890ae6e945c1987dd1fe9ef2394d
Linh Tran <linhtran@Linhs-MacBook-Air.local>
2023-03-20 20:18:23 -0700

    still debugging

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,25 @@
-const fs = require ("fs");
+const fs = require ('fs');
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
+// console.log(uuidv4());
 
 router.get('/api/notes', async (req, res) => {
-  const notes = JSON.parse(fs.readFileSync("./db/db.json","utf8"));
+  const notes = await JSON.parse(fs.readFileSync('./db/db.json'));
   res.json(notes);
 });
 
 router.post('/api/notes', (req, res) => {
-  const notes = JSON.parse(fs.readFileSync("./db/db.json","utf8"));
+  const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+  
   const addNote = {
     title: req.body.title,
     text: req.body.text,
     id: uuidv4(),
   };
+
   notes.push(addNote);
-  fs.writeFileSync("./db/db.json",JSON.stringify(notes));
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
   res.json(notes);
 });
-
-// router.delete('/api/notes/:id', (req, res) => {
-//   let data = fs.readFileSync("./db/db.json", "utf8");
-//   const dataJSON =  JSON.parse(data);
-//   const newNotes = dataJSON.filter((note) => { 
-//     return note.id !== req.params.id;
-//   });
-//   fs.writeFileSync("./db/db.json",JSON.stringify(newNotes));
-//   res.json("Note deleted.");
-// });
 
 module.exports = router; 
