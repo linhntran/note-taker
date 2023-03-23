20bb06e067d3485c619819da559745470579d33b
Linh Tran <linhtran@Linhs-Air.home>
2023-03-23 12:21:41 -0700

    fixed apiRoutes; had to re-load develop files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,18 @@
-const fs = require ('fs');
+const fs = require('fs');
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 // console.log(uuidv4());
 
+// Get request for saved notes from file
 router.get('/api/notes', async (req, res) => {
   const notes = await JSON.parse(fs.readFileSync('./db/db.json'));
   res.json(notes);
 });
 
+// Post request to create new note
 router.post('/api/notes', (req, res) => {
   const notes = JSON.parse(fs.readFileSync('./db/db.json'));
-  
+
   const addNote = {
     title: req.body.title,
     text: req.body.text,
@@ -20,6 +22,19 @@ router.post('/api/notes', (req, res) => {
   notes.push(addNote);
   fs.writeFileSync('./db/db.json', JSON.stringify(notes));
   res.json(notes);
+});
+
+// Delete request to remove existing note
+router.delete('/api/notes/:id', (req, res) => {
+  let data = fs.readFileSync("./db/db.json");
+  const dataJSON = JSON.parse(data);
+  
+  const deleteNotes = dataJSON.filter((note) => {
+    return note.id !== req.params.id;
+  });
+  
+  fs.writeFileSync("./db/db.json", JSON.stringify(deleteNotes));
+  res.json(deleteNotes);
 });
 
 module.exports = router; 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const path = require ("path");
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+// const path = require ('path');
+const apiRoutes = require('./routes/apiRoutes.js');
+const htmlRoutes = require('./routes/htmlRoutes.js');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -11,4 +11,4 @@ app.use(express.static('public'));
 app.use(apiRoutes);
 app.use(htmlRoutes);
 
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`));
